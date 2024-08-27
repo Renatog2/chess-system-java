2726cc6c934ee3aafdbaab7bb401bc661e559534
Renatog2 <r.g.moraes@hotmail.com>
2024-08-27 14:51:16 -0300

    Created class Board and Piece

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,11 +1,11 @@
 package Application;
 
-import bordgame.Position;
+import bordgame.Board;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Board board = new Board(8, 8);
 	}
 }
---
+++ src/bordgame/Piece.java
@@ -0,0 +1,19 @@
+package bordgame;
+
+public class Piece {
+	protected Position position;
+	private Board board;
+	
+	
+	//Constructors
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
+	
+	
+	//Getters and Setters
+	protected Board getBoard() {
+		return board;
+	}
+}
